72591ae4fa16bf538a764d06617ab3fa22cbff60
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-10-14 16:48:48 +0300

    complete what missed in authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   before_create  :set_default_role
   enum role: { admin: "admin", student: "student", instructor: "instructor" }
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
