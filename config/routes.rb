1f3b8b0c009df675e708e07e2f370cc5dfc2b254
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-10-25 15:41:23 +0300

    add Devise setup for basic User authentication Signup-login-logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+
+
+    # Devise routes for Users (customized paths)
+    devise_for :users, path: 'api/v1', path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    }, controllers: {
+      sessions: 'api/v1/users/sessions',
+      registrations: 'api/v1/users/registrations'
+    }
+  # namespace :api do
+    # namespace :v1 do
+    # end
+  # end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
