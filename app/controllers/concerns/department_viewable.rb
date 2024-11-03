a21982b2a698a4ad160965b87857c3bdf158d61e
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-11-03 22:08:34 +0200

    add Viewable file for department 'DRY'

---
+++ app/controllers/concerns/department_viewable.rb
@@ -0,0 +1,30 @@
+module DepartmentViewable
+  extend ActiveSupport::Concern
+  included do
+    before_action :set_department, only: [ :show ]
+  end
+
+  def index
+    @departments = Department.order(:name).page(params[:page]).per(params[:per_page].presence || 10)
+    render json: {
+      departments: @departments,
+      message: "All Departments fetched successfully",
+      meta: pagination_meta(@departments)
+    }, status: :ok
+  end
+
+  def show
+    render json: {
+      department: @department,
+      message: "Department Fetched successfully"
+    }, status: :ok
+  end
+
+  private
+
+  def set_department
+    @department = Department.find(params[:department_id])
+  rescue ActiveRecord::RecordNotFound
+    record_not_found('Department')
+  end
+end
