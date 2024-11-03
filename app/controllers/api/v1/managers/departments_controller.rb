4ea14d60c8facf23d7033bc4bf228f6738797274
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-11-03 22:36:41 +0200

    add Viewable file for department 'DRY' v2

--- app/controllers/api/v1/managers/departments_controller.rb
+++ app/controllers/api/v1/managers/departments_controller.rb
@@ -2,27 +2,12 @@ module Api
   module V1
     module Managers
       class DepartmentsController < ApplicationController
+        include DepartmentViewable
         before_action :authenticate_user!
         before_action :set_department, only: [ :update ]
 
         # He Can Read Departments Information.
         # He Can Update Departments Information.
-
-        def index
-          @departments = Department.page(params[:page]).per(params[:per_page].presence || 10)
-          render json: {
-            departments: @departments,
-            message: "All Departments Fetched Successfully",
-            meta: pagination_meta(@departments)
-          }, status: :ok
-        end
-
-        def show
-          render json: {
-            department: @department,
-            message: "Department Info. Fetched Successfully"
-          }, status: :ok
-        end
 
         def update
           if current_user.id == @department.manager_id
